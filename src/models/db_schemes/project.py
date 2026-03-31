9bbabec0f28c8c3d43ef57f2c101a1d06e322aa8
mahmoudwahman <mahmoudwahman006@gmail.com>
2026-03-31 11:13:37 +0200

    Continue-V10

---
+++ src/models/db_schemes/project.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel , Field, validator
+from typing import Optional
+from bson.objectid import ObjectId
+
+
+
+class Project(BaseModel):
+    _id: Optional[ObjectId]
+    project_id: str = Field(...,min_length=1, description="Unique identifier for the project")
+
+
+    # Custom validator to ensure project_id is alphanumeric
+    @validator('project_id')
+    def validate_project_id(cls, value):   # the cls present the class 
+        if not value.isalnum():
+            raise ValueError('project_id can only contain alphanumeric characters')
+        return value
+    
+
+    # This is necessary to allow ObjectId to be used as a field type in the Pydantic model (Pydantic do not understand ObjectId by default, so we need to allow arbitrary types)
+    class Config:        
+        arbitrary_types_allowed = True
